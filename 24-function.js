8743c1ad68d20773a3ebf6d57c0772b08dc172e3
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-06-17 19:01:28 +0000

    function practice code

--- 24-function.js
+++ 24-function.js
@@ -1,16 +1,26 @@
 // ================ function ===========
 
-function birthdaySong() {
-    console.log("Happy birthday too you baby...");
-}
+// function birthdaySong() {
+//     console.log("Happy birthday too you baby...");
+// }
 
-birthdaySong();
-birthdaySong();
+// birthdaySong();
+// birthdaySong();
 
 
 
-function sumTwo(num1, num2) {
-    console.log(num1 + num2);
-}
+// function sumTwo(num1, num2) {
+//     console.log(num1 + num2);
+// }
 
-sumTwo(10, 20);
+// sumTwo(10, 20);
+
+// function isEven(number) {
+//     if (number % 2 === 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// isEven(5);
+// console.log(isEven(67));
