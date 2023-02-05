7ac00c5e9ebe14a207d1637f34b7b13be1f86e4f
kailash kumar <kailash414243@gmail.com>
2023-02-05 23:44:42 +0530

    variables and string indexing practice in js

--- 2-variables.js
+++ 2-variables.js
@@ -20,3 +20,25 @@
 
 // var myName = "Aryan Singh";
 // console.log(myName);
+
+
+// console.log(myName);
+
+// function funcOne() {
+//     let myName = "Kailash Kumar";
+
+//     console.log(myName);
+// }
+
+// function funcTwo() {
+//     let myName = "Kailash";
+//     console.log(myName);
+// }
+
+// funcOne();
+// funcTwo();
+
+// const piValues = 22.7;
+// console.log(piValues);
+// const piValues = 20;
+// console.log(piValues);
--- 4-string-index.js
+++ 4-string-index.js
@@ -1,6 +1,18 @@
-let firstName = "kailash";
+// let firstName = "kailash";
 
-console.log(firstName[3]); //print third index of string
-console.log(firstName.length); //return lengnth of string
+// console.log(firstName[3]); //print third index of string
+// console.log(firstName.length); //return lengnth of string
 
-console.log(firstName[firstName.length - 1]) // print last string
+// console.log(firstName[firstName.length - 1]) // print last string
+
+let myStr = "Aryan";
+// console.log(myStr);
+
+// myStr[0];
+// console.log(myStr[0]);
+// myStr[0] = "k"; //string are immutable thats why you cannot change character but you can change entire string
+// console.log(myStr);
+
+// for (let i = 0; i < myStr.length; i++) {
+//     console.log(myStr[i]);
+// }
