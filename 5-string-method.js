e334dae78ac31c7ec81b7667b0b7c907e5f7edf7
kailash kumar <kailash414243@gmail.com>
2023-02-06 23:32:45 +0530

    js practice

--- 5-string-method.js
+++ 5-string-method.js
@@ -10,5 +10,32 @@ let firstName = "Kailash";
 // console.log(firstName.toUpperCase());
 // console.log(firstName.toLowerCase());
 
-let newString = firstName.slice(0, 5);
-console.log(newString);
+// let newString = firstName.slice(0, 5);
+// console.log(newString);
+
+let myVarOne = "kailash";
+// let myVarTwo = "kumar";
+// console.log(myVarOne);
+
+// --- length method --- 
+// console.log(myVarOne.length);
+// console.log(myVarTwo.length);
+
+// ----- slice method----
+
+// let sliceResult = myVarOne.slice(0, 3);
+// console.log(sliceResult);
+
+// ---- trim method -----
+// console.log(myVarOne.length);
+// let trimResult = myVarOne.trim();
+// console.log(trimResult.length);
+
+// ------ replace method ----------
+
+// let replaceResult = myVarOne.replace("a", "B");
+// console.log(replaceResult);
+
+// ------ replace all method ------- 
+// let replaceAllResult = myVarOne.replaceAll('a', "S");
+// console.log(replaceAllResult);
