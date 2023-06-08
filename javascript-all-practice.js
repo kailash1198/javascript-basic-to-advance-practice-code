e26c4749ea5978b94a91436ca25839e5653cc809
kailash kumar <kailash414243@gmail.com>
2023-06-08 22:02:13 +0530

    practice js

---
+++ javascript-all-practice.js
@@ -0,0 +1,81 @@
+// ------- test code ------
+/*
+console.log("Hello world");
+console.log("Congrat, Kailash, Now you can go other topics for master in Javascript language");
+
+
+*/
+// ===================================================
+//                   1.       VARIABLES
+// ====================================================
+
+/*
+var numOne = 10;
+var numTwo = 20;
+
+var totalNum = numOne + numTwo;
+console.log(totalNum);
+
+var firstName = "Kailash";
+var lastName = "Kumar";
+
+fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var trueAns = true;
+var falseAns = false;
+
+if(trueAns == true && falseAns == false){
+    console.log("You are Right Kailash");
+}
+
+
+
+let myName = "Kailash Kumar";
+const myAge = 20;
+
+if(myName == "Kailash Kumar" && myAge == 20){
+    console.log("You are eligible to Admission in this college");
+}
+
+
+
+function test() {
+  if (true) {
+    var myName = "Kailash";
+    console.log(myName);
+  }
+}
+
+test(myName);
+
+function example() {
+    if (true) {
+      var x = 5;
+      console.log(x); // Output: 5
+    }
+    console.log(x); // Output: 5
+  }
+
+  example();
+  console.log(x); // Throws an error: ReferenceError: x is not defined
+
+*/
+
+// ============================================================
+//                    2.       DATA TYPES
+// ============================================================
+
+let numOne = 20;
+let numTwo = 10.5;
+let numThree = -10;
+
+console.log(numOne);
+console.log(numTwo);
+console.log(numThree);
+
+console.log(numTwo / 0);
+
+let firstName = "Kailash";
+let lastName = "Kumar";
+
