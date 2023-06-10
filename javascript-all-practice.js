9f6e067273e7e5706e497207abd60f3656df2624
kailash kumar <kailash414243@gmail.com>
2023-06-10 23:16:54 +0530

    js practice

--- javascript-all-practice.js
+++ javascript-all-practice.js
@@ -66,16 +66,121 @@ function example() {
 //                    2.       DATA TYPES
 // ============================================================
 
-let numOne = 20;
-let numTwo = 10.5;
-let numThree = -10;
+// let numOne = 20;
+// let numTwo = 10.5;
+// let numThree = -10;
 
-console.log(numOne);
-console.log(numTwo);
-console.log(numThree);
+// console.log(numOne);
+// console.log(numTwo);
+// console.log(numThree);
 
-console.log(numTwo / 0);
+// console.log(numTwo / 0);
 
-let firstName = "Kailash";
-let lastName = "Kumar";
+// let firstName = "Kailash";
+// let lastName = "Kumar";
 
+
+// let myName = "Kailash";
+// console.log(typeof myName);
+
+// let myNum = 82833348757834678345643;
+// console.log(myNum);
+// console.log(typeof myNum);
+
+// let ansReply = true;
+// console.log(typeof ansReply);
+
+// if(ansReply==true){
+//   function greet(){
+//     console.log("Hello world");
+//   }
+
+//   greet();
+// }
+
+
+// let myNum;
+
+// console.log(typeof myNum);
+
+// const myObject = {
+//   userName : "Kailash",
+//   userAge : 24,
+//   userAddress : "Bangalore"
+
+// }
+
+// console.log(myObject.userName);
+// console.log(myObject.userAddress);
+
+
+// let myArray = [10, 11,12];
+
+// console.log(myArray);
+
+// console.log(myArray[1]);
+
+
+
+// function myFunc(a,b){
+//   return a+b;
+// }
+
+// let funcValue = myFunc(10,11);
+
+// console.log(funcValue);
+
+
+
+
+// ============================================================
+//                    3.      OPERATORS
+// ============================================================
+
+// let numOne = 30, numTwo = 20;
+// console.log(numOne + numTwo);
+// console.log(numOne%numTwo);
+
+// let newNum = numOne +=numTwo;
+// console.log(newNum);
+// console.log(numOne);
+
+// if(numOne>=numTwo){
+//   console.log("Num one greater than num two");
+// }
+
+
+// ============================================================
+//                    4.      IF ELSE STATEMENTS
+// ============================================================
+
+// let numOne = 11;
+
+// if(numOne<12){
+//   console.log("Number less then 12");
+// }else if(numOne==10){
+//   console.log("Number is equal to 19");
+// }
+
+// let userOneAge = 18, userTwoAge = 20, userThreeAge = 30;
+// if(userOneAge==18){
+//   if(userTwoAge==20){
+//     if(userThreeAge==30){
+//       console.log("You are BIHARI HERO");
+//       if(userOneAge == 18 && userTwoAge<=30){
+//         console.log("BIHARI NUMBER - 2");
+//       }
+//     }
+//   }
+// }
+
+// ============================================================
+//                    5.      LOOPS
+// ============================================================
+
+let myNum = 10;
+
+// while (myNum<=20) {
+//   console.log(myNum);
+//   myNum++;
+// }
